30bf5e1597262f8c8f8a7ab9eebe1e35f1052941
Unknown <mythologyali@gmail.com>
2018-09-29 06:48:56 -0700

    request validity checker

--- src/ingest/resolvers/__init__.py
+++ src/ingest/resolvers/__init__.py
@@ -1,2 +1,8 @@
+from requests import Response
+
 ANILIST_ENDPOINT = 'https://graphql.anilist.co'
 MAL_HINT_ENDPOINT = 'https://myanimelist.net/search/prefix.json?type=anime&keyword='
+
+
+def is_request_successful(response: Response) -> bool:
+    return response.status_code[0] == 2
