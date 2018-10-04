c33e38b7eb73c1a581f97d9935187dfddcedb97c
ilocereal <mythologyali@gmail.com>
2018-10-04 15:05:16 -0700

    now checking for python variable before trying to access Redis

--- worker/api/cache.py
+++ worker/api/cache.py
@@ -1,7 +1,14 @@
 import redis
+import os 
+import logging
 
+logger = logging.getLogger(__name__)
 
 def setup_redis():
+    if os.getenv('PYTHON_ENV', 'DEBUG') == 'DEBUG':
+        logger.warning('Python is running in debug mode, Redis is disabled')
+        return None
+
     return redis.StrictRedis(
         host='localhost',
         port=6379,
