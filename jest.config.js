f91fa0771ce346d32159c16d063e110eeb026a2d
Unknown <mythologyali@gmail.com>
2018-10-18 04:14:50 -0700

    jest setup

---
+++ jest.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+	projects: [
+		{
+			displayName: "worker",
+			// runner: "jest-runner-tsc",
+			rootDir: "dist/typescript-worker/tests"
+		}, {
+			displayName: "api"
+		}
+	]
+}
