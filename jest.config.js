3c0b06baba2dabfe8e3e7cb8641b7574745c26cd
Xetera <mythologyali@gmail.com>
2018-10-23 20:37:26 -0700

    split up modules for jest

--- jest.config.js
+++ jest.config.js
@@ -1,13 +1,6 @@
 module.exports = {
-	projects: [
-		{
-			displayName: "worker",
-			// runner: "jest-runner-tsc",
-			rootDir: "dist/typescript-worker/__tests__",
-			testEnvironment: "node"
-		}, {
-			displayName: "api"
-		}
-	],
-	testEnvironment: "node"
-}
+  projects: [
+    "jest.worker.js"
+  ]
+};
+
