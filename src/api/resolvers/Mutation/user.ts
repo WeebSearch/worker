e3af176a87367a1851a56635d000c7ef22062553
Xetera <mythologyali@gmail.com>
2019-01-12 12:24:19 -0800

    hasura dep'

--- src/api/resolvers/Mutation/user.ts
+++
@@ -1,7 +0,0 @@
-import { Context } from "../../utils";
-
-export const userMutation = {
-  async createUser(_, args, ctx: Context, info) {
-    return ctx.db.mutation.createUser({ data: args });
-  }
-};
